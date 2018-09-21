25a97863d730d5178b623533f2b1ba79bda630ca
Israel Calderon <caldz@Israels-MacBook-Air.local>
2018-09-21 19:40:54 +0800

    chore: Modified recipe and reformat the style of code

--- contracts/Recipes.sol
+++ contracts/Recipes.sol
@@ -2,61 +2,40 @@ pragma solidity ^0.4.24;
 
 contract Recipes {
     struct Recipe {
-        string ipfsHash;
-        address owner;
+        mapping ( address => bool) allowedToView;
+        uint etherAmount;
         uint index;
-        uint timeCreated;
+        string ipfsHash;
         string recipeType;
         string origin;
-        uint etherAmount;
-        mapping ( address => bool) allowedToView;
+        address owner;
+        uint timeCreated;
     }
 
-    mapping(uint => Recipe) recipes;
     mapping (address => uint) balances;
-    mapping ( address => Recipe[]) usersTransactions;
-    
-    uint[] recipeIndexes;
     address private owner;
+    mapping ( address => Recipe[]) usersTransactions;
+    mapping(uint => Recipe) recipes;
+    uint[] recipeIndexes;
 
+    modifier isEqualAmount(uint index) {
+        require(recipes[index].etherAmount == msg.value);
+        _; 
+    }
+    
+    modifier indexExist(uint _index) {
+        require(_index < recipeIndexes.length);
+        _;
+    }
+    
     modifier isRecipeOwner(uint _index) {
         require(recipes[_index].owner == msg.sender || owner == msg.sender);
         _;
     }
 
-    modifier indexExist(uint _index) {
-        require(_index < recipeIndexes.length);
+    modifier userTransactionIndexExist(uint index) {
+        require(index < usersTransactions[msg.sender].length);
         _;
-    }
-
-    modifier isEqualAmount(uint index) {
-        require(recipes[index].etherAmount == msg.value, "Amount is not the same");
-        _; 
-    }
-
-    function isAllowedToView(uint recipeIndex)  public view indexExist(recipeIndex) returns (bool) { 
-        bytes32 restrictedKey = keccak256("payable");
-        bytes32 dataType = keccak256(recipes[recipeIndex].recipeType);
-        if (restrictedKey == dataType) {
-            return recipes[recipeIndex].allowedToView[msg.sender];
-        }
-        return true;
-    }
-
-    function buySubscription(uint _index) public indexExist(_index) isEqualAmount(_index) payable {
-        allowUserToView(_index);
-        address ownerAddress =  recipes[_index].owner;
-        balances[ownerAddress] += msg.value; 
-    }
-
-    function getBalance() public view returns(uint balance) {
-        return balances[msg.sender];
-    } 
-
-    function cashOut() public {
-        uint amount = balances[msg.sender];
-        balances[msg.sender] = 0;
-        msg.sender.transfer(amount);
     }
 
     function addRecipe(string hash, string recipeType, string origin, uint amount) public {
@@ -75,16 +54,70 @@ contract Recipes {
         recipes[recipeIndex].allowedToView[msg.sender] = true;
         usersTransactions[msg.sender].push(recipes[recipeIndex]);
     }
+    
+    function isAllowedToView(uint recipeIndex)  public view indexExist(recipeIndex) returns (bool) { 
+        bytes32 restrictedKey = keccak256("payable");
+        bytes32 dataType = keccak256(recipes[recipeIndex].recipeType);
+        if (restrictedKey == dataType) {
+            return recipes[recipeIndex].allowedToView[msg.sender];
+        }
+        return true;
+    }
+
+    function buySubscription(uint _index) public indexExist(_index) isEqualAmount(_index) payable {
+        allowUserToView(_index);
+        address ownerAddress =  recipes[_index].owner;
+        balances[ownerAddress] += msg.value; 
+    }
+   
+    function cashOut() public {
+        uint amount = balances[msg.sender];
+        balances[msg.sender] = 0;
+        msg.sender.transfer(amount);
+    }
+   
+    function deleteIndex(uint index) public indexExist(index) isRecipeOwner(index) {
+        uint lastIndex = recipeIndexes.length - 1;
+        uint currentIndexValue = recipeIndexes[index];
+        uint lastIndexValue = recipeIndexes[lastIndex];
+        recipeIndexes[index] = lastIndexValue;
+        recipeIndexes[lastIndex] = currentIndexValue;
+        recipeIndexes.length--;
+        recipes[index] = recipes[lastIndex];
+        delete recipes[lastIndex];
+    }
+
+    function getBalance() public view returns(uint balance) {
+        return balances[msg.sender];
+    } 
 
     function getRecipeCount() public view returns(uint) {
         return recipeIndexes.length;
     }
-
-    modifier userTransactionIndexExist(uint index) {
-        require(index < usersTransactions[msg.sender].length, "Index doesnt exist");
-        _;
+    
+    function getRecipeIndexesAt(uint index) public view returns(uint) {
+        return recipeIndexes[index];
     }
 
+    function getSpecificIndexRecipe(uint index) public view indexExist(index) 
+        returns(
+            address owner, 
+            string ipfsHash, 
+            string recipeType, 
+            uint timeCreated, 
+            string origin, 
+            uint amount
+        ) {
+            return (
+                recipes[index].owner,
+                recipes[index].ipfsHash,
+                recipes[index].recipeType,
+                recipes[index].timeCreated,
+                recipes[index].origin,
+                recipes[index].etherAmount
+            );
+    }
+    
     function getUserTransactions(uint _index) public view userTransactionIndexExist(_index) 
         returns (address owner, string ipfsHash, string recipeType, uint timeCreated, string origin, uint amount) 
     {
@@ -97,33 +130,5 @@ contract Recipes {
             recipe.origin,
             recipe.etherAmount
         );
-    }
-
-    function getSpecificIndexRecipe(uint index) public view indexExist(index) 
-    returns(address owner, string ipfsHash, string recipeType, uint timeCreated, string origin, uint amount) 
-    {
-        return (
-            recipes[index].owner,
-            recipes[index].ipfsHash,
-            recipes[index].recipeType,
-            recipes[index].timeCreated,
-            recipes[index].origin,
-            recipes[index].etherAmount
-        );
-    }
-
-    function getRecipeIndexesAt(uint index) public view returns(uint) {
-        return recipeIndexes[index];
-    }
-
-    function deleteIndex(uint index) public indexExist(index) isRecipeOwner(index) {
-        uint lastIndex = recipeIndexes.length - 1;
-        uint currentIndexValue = recipeIndexes[index];
-        uint lastIndexValue = recipeIndexes[lastIndex];
-        recipeIndexes[index] = lastIndexValue;
-        recipeIndexes[lastIndex] = currentIndexValue;
-        recipeIndexes.length--;
-        recipes[index] = recipes[lastIndex];
-        delete recipes[lastIndex];
     }
 }
